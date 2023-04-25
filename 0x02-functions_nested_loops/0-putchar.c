26c4ddf39d457056a7ba8205de33d21c6f3232f7
Lebohang <lgmajalle@gmail.com>
2023-04-25 14:41:11 -0700

    now

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - Starting point
  * Description- 'Prints _putchar to stdout'
- * Return: 0 to indicate successful execution 
+ * Return:0 to indicate successful execution 
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return(0);
 }
