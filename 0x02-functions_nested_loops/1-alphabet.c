20b701b0fa045672b07d4d3db5928ee6341f08af
Lebohang <lgmajalle@gmail.com>
2023-04-25 15:32:48 -0700

    commit 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *Description -Prints the alphabet in lowercase.
+ *
+ *Return: 0 to indicate successful execution
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i<= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
